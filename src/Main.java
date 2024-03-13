4b2638ee2b812b1f67e2d869a671dfe972776133
yousr <yousraguettaf60@gmail.com>
2024-03-13 09:41:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+ IntelliJ IDEA;
+        out/
+                !**/src/main/**/out/
+                !**/src/test/**/out/
+
+Eclipse
+.apt_generated
+                .classpath
+                .factorypath
+                .project
+                .settings
+                .springBeans
+                .sts4-cache
+        bin/
+                !**/src/main/**/bin/
+                !**/src/test/**/bin/
+
+ NetBeans
+/nbproject/private/
+/nbbuild/
+/dist/
+/nbdist/
+/.nb-gradle/
+
+VS Code
+.vscode/
+
+ Mac OS
+.DS_Store
+        }
+    }
+}
